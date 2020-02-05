49af7c35471695013782d8d64ce1dcec7cad681f
Edwin18 <igreenbergi78@gmail.com>
2020-02-05 20:35:30 +0200

    Реализует компонент Main

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App.jsx';
+
+const AVAILABLE = 459;
+
+ReactDOM.render(
+    <App
+      available = {AVAILABLE}
+    />,
+    document.querySelector(`#root`)
+);
