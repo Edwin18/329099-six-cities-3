2c648d2b601df3e6487d66029539154678771011
Edwin18 <igreenbergi78@gmail.com>
2020-02-14 04:14:02 +0200

    Реализует пункт задания 'Подготовка'

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main/main.jsx';
+import Property from '../property/property.jsx';
 
 const headingLinkHandler = () => {};
 
-const App = ({available, offers}) => (
-  <Main
-    available={available}
-    offers={offers}
-    onHeadingLinkClick={headingLinkHandler}
-  />
-);
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-property">
+            <Property />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  _renderApp() {
+    const {available, offers} = this.props;
+
+    return (
+      <Main
+        available={available}
+        offers={offers}
+        onHeadingLinkClick={headingLinkHandler}
+      />
+    );
+  }
+}
 
 App.propTypes = {
   available: PropTypes.number.isRequired,
