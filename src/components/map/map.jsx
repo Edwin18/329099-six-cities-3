670650b41d6ffe1379f5bb0317fbf2730c0ea1b4
Edwin18 <igreenbergi78@gmail.com>
2020-02-22 16:32:42 +0200

    Рефакторит компонент 'map'.

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -6,7 +6,8 @@ class Map extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this._map = createRef();
+    this._map = null;
+    this._mapContainer = createRef();
   }
 
   componentDidMount() {
@@ -15,35 +16,65 @@ class Map extends React.PureComponent {
     const city = [52.38333, 4.9];
     const zoom = 12;
 
-    const icon = leaflet.icon({
-      iconUrl: `img/pin.svg`,
-      iconSize: [30, 30]
-    });
+    this._initMap(city, zoom, coordinates);
+  }
 
-    const map = leaflet.map(this._map.current, {
+  componentWillUnmount() {
+    this._map = null;
+  }
+
+  render() {
+    return <div id="map" style={{height: `100%`}} ref={this._mapContainer}></div>;
+  }
+
+  _initMap(city, zoom, coordinates) {
+    this._createMap(city, zoom);
+    this._setView(city, zoom);
+    this._setLayer();
+    this._renderIcons(coordinates);
+  }
+
+  _createMap(city, zoom) {
+    this._map = leaflet.map(this._mapContainer.current, {
       center: city,
       zoom,
       zoomControl: false,
       marker: true
     });
+  }
 
-    map.setView(city, zoom);
+  _setView(city, zoom) {
+    this._map.setView(city, zoom);
+  }
 
+  _setLayer() {
     leaflet
-      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-      })
-      .addTo(map);
+    .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+    })
+    .addTo(this._map);
+  }
 
+  _renderIcons(coordinates) {
     coordinates.map((coordinate) => (
       leaflet
-        .marker(coordinate, {icon})
-        .addTo(map)
+        .marker(coordinate, {icon: this._getDefaultIcon()})
+        .addTo(this._map)
     ));
   }
 
-  render() {
-    return <div id="map" style={{height: `100%`}} ref={this._map}></div>;
+  _getDefaultIcon() {
+    return leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
+  }
+
+  _getActiveIcon() {
+    return leaflet.icon({
+      iconUrl: `img/pin-active.svg`,
+      iconSize: [30, 30]
+    });
   }
 }
 
